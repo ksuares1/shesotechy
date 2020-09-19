e9d31de8b8784b5bbd960bf375ae1b0bf76b44c4
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-09-19 15:16:23 -0400

    added row and blog card

--- shesotechy/src/components/Home/index.js
+++ shesotechy/src/components/Home/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import './index.css';
 import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import { MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText } from 'mdbreact';
 
 const HomePage = () => {
   return (
+      <main>
+      <div class="container">
     <MDBContainer className="mt-5 text-center">
       <MDBRow>
         <MDBCol>
@@ -12,11 +15,8 @@ const HomePage = () => {
             <h3 className="lead">
                 Discover the inner techy in you!
               </h3>
-            <hr className="my-2" />
-            <p>
-              It uses utility classes for typgraphy and spacing to space content out
-              within the larger container.
-            </p>
+           <br>
+           </br>
             <p className="lead">
               <MDBBtn color="primary">Learn More</MDBBtn>
             </p>
@@ -24,7 +24,29 @@ const HomePage = () => {
         </MDBCol>
       </MDBRow>
     </MDBContainer>
+</div>
 
+<br>
+</br>
+<br>
+</br>
+<div className="row">
+<MDBCol>
+  <h1 className="blog-title">ShesoTechy Blog</h1> 
+      <MDBCard style={{ width: "22rem" }}>
+        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
+        <MDBCardBody>
+          <MDBCardTitle>Card title</MDBCardTitle>
+          <MDBCardText>
+            Some quick example text to build on the card title and make
+            up the bulk of the card&apos;s content.
+          </MDBCardText>
+          <MDBBtn href="#">MDBBtn</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
+    </div>
+    </main>
 
     )
 }
