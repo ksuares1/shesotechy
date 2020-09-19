5298037aaa9eb176287a6e75b52647e659f9cf9e
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-09-18 23:38:57 -0400

    Added code to home and app

--- shesotechy/src/App.js
+++ shesotechy/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/Nav';
+import HomePage from './components/Home';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,8 +12,8 @@ function App() {
         <div>
             <Navbar/>  
           <div>
-            {/* <Route exact path="/" component={HomeComponent} />
-            <Route exact path="/about" component={AboutComponent} /> */}
+            <Route exact path="/" component={HomePage} />
+            {/* <Route exact path="/about" component={AboutComponent} /> */} 
             {/* <Route exact path="/flute" component={FluteRepairs} /> */}
             {/* <Route exact path="/contact" component={ContactPage} />
              <Route exact path="/blog" component={BlogComponent} /> */}
--- shesotechy/src/components/Home/index.js
+++ shesotechy/src/components/Home/index.js
@@ -1,11 +1,30 @@
 import React from "react";
 import './index.css';
+import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
-function HomePage(){
-    return(
-        <div>
-            
-        </div>
+const HomePage = () => {
+  return (
+    <MDBContainer className="mt-5 text-center">
+      <MDBRow>
+        <MDBCol>
+          <MDBJumbotron>
+            <h2 className="h1 display-3">Welcome to ShesoTechy</h2>
+            <h3 className="lead">
+                Discover the inner techy in you!
+              </h3>
+            <hr className="my-2" />
+            <p>
+              It uses utility classes for typgraphy and spacing to space content out
+              within the larger container.
+            </p>
+            <p className="lead">
+              <MDBBtn color="primary">Learn More</MDBBtn>
+            </p>
+          </MDBJumbotron>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+
 
     )
 }
