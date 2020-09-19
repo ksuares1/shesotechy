f9ac4fb4e06e53141168b28a81d29683e5c9747d
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-09-18 22:31:25 -0400

    added home component code

--- shesotechy/src/components/Nav/index.js
+++ shesotechy/src/components/Nav/index.js
@@ -3,9 +3,7 @@ import "./index.css";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+
 
 
 function NavBar() {
@@ -24,10 +22,6 @@ function NavBar() {
               <NavDropdown.Item href="/blog">Blog</NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button> 
-          </Form>
         </Navbar.Collapse>
       </Navbar>
 
